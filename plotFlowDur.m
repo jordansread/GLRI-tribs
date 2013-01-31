895c03ee0e7af800e554e08cd50b7fbfdd6af2fa
Jordan S Read <jread@usgs.gov>
2013-01-31 09:43:27 -0600

    seasonal data plots w bounds

--- plotFlowDur.m
+++ plotFlowDur.m
@@ -4,8 +4,6 @@ function plotFlowDur(pCode)
 % plots flow duration curves with indicators for samples of X (X denoted by
 % parameter coded [pCode])
 
-figSize = 'large';
-figRes  = struct('large','-r500','medium','-r200','small','-r100');
 plotGen = 4;
 axLw = 1.25;
 mF = [.7 .85 .85];
@@ -26,6 +24,8 @@ hSpc = 1;  % vertical space
 W    = (figW-lM-rM-wSpc)/2;
 H    = (figH-tM-bM-hSpc)/2;
 
+GLRI_st = datenum('2011-01-01');
+GLRI_en = now;
 
 if eq(nargin,0)
     pCode = '00665';
@@ -35,9 +35,9 @@ if ~ischar(pCode)
     error('pCode input must be string');
 end
 distPcode = '00060';
-startDT = '2010-10-01';
+startDT = '1900-10-01';
 yScale = 'log';
-figDir = 'I:\GLRI Tribs\Figures\';
+figFldr = 'Figures/SeasonalFlowDur/';
 rootDir = ['/Volumes/projects/QW Monitoring Team/GLRI toxics/'...
     'Data Analysis/'];
 %sondFldr= 'Data\Site Data\'; 
@@ -48,7 +48,7 @@ combineSID = {'04157000' '04157005'; '04193500' '04193490'};
 
 siteIDs = {'04024000' '04027000' '04040000' '04067500' '04059500' ...
     '040851385' '04085427' '04087170' '04092750' '04095090' '04101500' ...
-    '04108660' '04119400' '04121970' '04137500' '04142000' '04157000' ...
+    '04108660' '04121970' '04137500' '04142000' '04157000' ... % removed '04119400'
     '04165500' '04166500' '04174500' '04176500' '04193500' '04195500' ...
     '04200500' '04199500' '04208000' '04213500' '04231600' '04249000' ...
     '04269000'};
@@ -121,28 +121,72 @@ for k = 1:numSites
     nanI = isnan(Qdaily);
     Qdates = Qdates(~nanI);
     Qdaily = Qdaily(~nanI);
-    [srtQ,srtI] = sort(-Qdaily); % reverse sort
+    
+    % now break into seasons
+    
+   
+    
+    dVec = datevec(Qdates);
+    yrStripQ = Qdates-datenum(dVec(:,1),1,0); % no years, just day of year
+    dVec = datevec(dates);
+    yrStripP = dates- datenum(dVec(:,1),1,0);
+    useI = ge(Qdates,GLRI_st) & lt(Qdates,GLRI_en);
+    seasonTrim = Qdaily(useI);
+    seasonTrimD= Qdates(useI);
+    dVec = datevec(seasonTrimD);
+    yrStripS = seasonTrimD- datenum(dVec(:,1),1,0);
+    
+    
+    %% first date range: 
+    
+    useI = true(length(yrStripQ),1);
+    useIp = ge(yrStripP,datenum('0-12-21','yyyy-mm-dd')) | ...
+        lt(yrStripP,datenum('0-03-20','yyyy-mm-dd'));
+    useIs = ge(yrStripS,datenum('0-12-21','yyyy-mm-dd')) | ...
+        lt(yrStripS,datenum('0-03-20','yyyy-mm-dd'));
+    Qtmp = Qdaily(useI);
+    Qdts = Qdates(useI);
+    [srtQ,srtI] = sort(-Qtmp); % reverse sort
     srtQ = -srtQ;
-    Qdates = Qdates(srtI);
+    Qdts = Qdts(srtI);
+    tDts = dates(useIp);
+    
     xNumz = linspace(0,100,length(srtQ));
     plot(xNumz,srtQ,'Parent',ax_h(plotCnt),'LineWidth',LW)
     % now...find the sampling locations
     
     N = 0;
-    for j = 1:length(dates)
-        plotI = eq(dates(j),Qdates);
+    for j = 1:length(tDts)
+        plotI = eq(tDts(j),Qdts);
         plot(xNumz(plotI),srtQ(plotI),'ro',...
             'MarkerSize',mS,'MarkerFaceColor',mF,...
             'Parent',ax_h(plotCnt),'LineWidth',.75)
         N = N + sum(plotI);
-        
     end
-    P = length(dates);
-    try siteName = getSiteName(siteIDs{k});
-    catch issue
-        disp(['site name ' siteIDs{k} ' not found']);
-        siteName = '';
-    end
+    % GLRI range plot
+    useI = ge(Qdates,GLRI_st) & lt(Qdates,GLRI_en);
+    mxI  = max(Qdaily(useI));
+    mnI  = min(Qdaily(useI));
+    
+    plotI_1 = find(srtQ==mxI,1,'first');
+    plotI_2 = find(srtQ==mnI,1,'first');
+    plot(xNumz(plotI_1),mxI,'kv','Parent',ax_h(plotCnt),'LineWidth',.75,...
+        'MarkerFaceColor','w','MarkerSize',6)
+    plot(xNumz(plotI_2),mnI,'k^','Parent',ax_h(plotCnt),'LineWidth',.75,...
+        'MarkerFaceColor','w','MarkerSize',6)
+    
+    mxI  = max(seasonTrim(useIs));
+    mnI  = min(seasonTrim(useIs));
+    plotI_1 = find(srtQ==mxI,1,'first');
+    plotI_2 = find(srtQ==mnI,1,'first');
+    plot(xNumz(plotI_1),mxI,'kx','Parent',ax_h(plotCnt),'LineWidth',1.25,...
+        'MarkerSize',9)
+    plot(xNumz(plotI_2),mnI,'kx','Parent',ax_h(plotCnt),'LineWidth',1.25,...
+        'MarkerSize',9)
+    
+    
+    P = length(tDts);
+    siteName = 'Winter';
     if gage
         set(get(ax_h(plotCnt),'YLabel'),'String','Gage height (ft)')
         set(ax_h(plotCnt),'YScale','linear')
@@ -171,11 +215,248 @@ for k = 1:numSites
                 'Parent',ax_h(plotCnt));
         end
     end
+    plotCnt = plotCnt+1;
+    %% spring
+    useI = true(length(yrStripQ),1);
+    useIp = ge(yrStripP,datenum('0-03-20','yyyy-mm-dd')) & ...
+        lt(yrStripP,datenum('0-06-21','yyyy-mm-dd'));
+    useIs = ge(yrStripS,datenum('0-03-20','yyyy-mm-dd')) & ...
+        lt(yrStripS,datenum('0-06-21','yyyy-mm-dd'));
+    Qtmp = Qdaily(useI);
+    Qdts = Qdates(useI);
+    [srtQ,srtI] = sort(-Qtmp); % reverse sort
+    srtQ = -srtQ;
+    Qdts = Qdts(srtI);
+    tDts = dates(useIp);
+    
+    xNumz = linspace(0,100,length(srtQ));
+    plot(xNumz,srtQ,'Parent',ax_h(plotCnt),'LineWidth',LW)
+    % now...find the sampling locations
+    
+    N = 0;
+    for j = 1:length(tDts)
+        plotI = eq(tDts(j),Qdts);
+        plot(xNumz(plotI),srtQ(plotI),'ro',...
+            'MarkerSize',mS,'MarkerFaceColor',mF,...
+            'Parent',ax_h(plotCnt),'LineWidth',.75)
+        N = N + sum(plotI);
+        
+    end
+    % GLRI range plot
+    useI = ge(Qdates,GLRI_st) & lt(Qdates,GLRI_en);
+    mxI  = max(Qdaily(useI));
+    mnI  = min(Qdaily(useI));
+    
+    plotI_1 = find(srtQ==mxI,1,'first');
+    plotI_2 = find(srtQ==mnI,1,'first');
+    plot(xNumz(plotI_1),mxI,'kv','Parent',ax_h(plotCnt),'LineWidth',.75,...
+        'MarkerFaceColor','w','MarkerSize',6)
+    plot(xNumz(plotI_2),mnI,'k^','Parent',ax_h(plotCnt),'LineWidth',.75,...
+        'MarkerFaceColor','w','MarkerSize',6)
+    
+    mxI  = max(seasonTrim(useIs));
+    mnI  = min(seasonTrim(useIs));
+    plotI_1 = find(srtQ==mxI,1,'first');
+    plotI_2 = find(srtQ==mnI,1,'first');
+    plot(xNumz(plotI_1),mxI,'kx','Parent',ax_h(plotCnt),'LineWidth',1.25,...
+        'MarkerSize',9)
+    plot(xNumz(plotI_2),mnI,'kx','Parent',ax_h(plotCnt),'LineWidth',1.25,...
+        'MarkerSize',9)
+    
+    
+    P = length(tDts);
+    siteName = 'Spring';
+    if gage
+        set(get(ax_h(plotCnt),'YLabel'),'String','Gage height (ft)')
+        set(ax_h(plotCnt),'YScale','linear')
+    end
+    if cmb
+        if ne(P,N)
+            title([siteName ' (' siteIDs{k} ' & ' char(comSID) ...
+                '; ' 'N=' num2str(N) ...
+                '; {\color{red}total=' num2str(P) '})'],...
+                'Parent',ax_h(plotCnt));
+        else
+            title([siteName ' (' siteIDs{k} ' & ' char(comSID) ...
+                '; ' 'N=' num2str(N) ...
+                ')'],...
+                'Parent',ax_h(plotCnt));
+        end
+        
+    else
+        if ne(P,N)
+            title([siteName ' (' siteIDs{k} '; N=' num2str(N) ...
+                '; {\color{red}total=' num2str(P) '})'],...
+                'Parent',ax_h(plotCnt));
+        else
+            title([siteName ' (' siteIDs{k} '; N=' num2str(N) ...
+                ')'],...
+                'Parent',ax_h(plotCnt));
+        end
+    end
+    plotCnt = plotCnt+1;
+    %% summer
+    useI = true(length(yrStripQ),1);
+    useIp = ge(yrStripP,datenum('0-06-21','yyyy-mm-dd')) & ...
+        lt(yrStripP,datenum('0-09-22','yyyy-mm-dd'));
+    useIs = ge(yrStripS,datenum('0-06-21','yyyy-mm-dd')) & ...
+        lt(yrStripS,datenum('0-09-22','yyyy-mm-dd'));
+    Qtmp = Qdaily(useI);
+    Qdts = Qdates(useI);
+    [srtQ,srtI] = sort(-Qtmp); % reverse sort
+    srtQ = -srtQ;
+    Qdts = Qdts(srtI);
+    tDts = dates(useIp);
+    
+    xNumz = linspace(0,100,length(srtQ));
+    plot(xNumz,srtQ,'Parent',ax_h(plotCnt),'LineWidth',LW)
+    % now...find the sampling locations
+    
+    N = 0;
+    for j = 1:length(tDts)
+        plotI = eq(tDts(j),Qdts);
+        plot(xNumz(plotI),srtQ(plotI),'ro',...
+            'MarkerSize',mS,'MarkerFaceColor',mF,...
+            'Parent',ax_h(plotCnt),'LineWidth',.75)
+        N = N + sum(plotI);
+        
+    end
+    % GLRI range plot
+    useI = ge(Qdates,GLRI_st) & lt(Qdates,GLRI_en);
+    mxI  = max(Qdaily(useI));
+    mnI  = min(Qdaily(useI));
+    
+    plotI_1 = find(srtQ==mxI,1,'first');
+    plotI_2 = find(srtQ==mnI,1,'first');
+    plot(xNumz(plotI_1),mxI,'kv','Parent',ax_h(plotCnt),'LineWidth',.75,...
+        'MarkerFaceColor','w','MarkerSize',6)
+    plot(xNumz(plotI_2),mnI,'k^','Parent',ax_h(plotCnt),'LineWidth',.75,...
+        'MarkerFaceColor','w','MarkerSize',6)
+    
+    mxI  = max(seasonTrim(useIs));
+    mnI  = min(seasonTrim(useIs));
+    plotI_1 = find(srtQ==mxI,1,'first');
+    plotI_2 = find(srtQ==mnI,1,'first');
+    plot(xNumz(plotI_1),mxI,'kx','Parent',ax_h(plotCnt),'LineWidth',1.25,...
+        'MarkerSize',9)
+    plot(xNumz(plotI_2),mnI,'kx','Parent',ax_h(plotCnt),'LineWidth',1.25,...
+        'MarkerSize',9)
+    P = length(tDts);
+    siteName = 'Summer';
+    if gage
+        set(get(ax_h(plotCnt),'YLabel'),'String','Gage height (ft)')
+        set(ax_h(plotCnt),'YScale','linear')
+    end
+    if cmb
+        if ne(P,N)
+            title([siteName ' (' siteIDs{k} ' & ' char(comSID) ...
+                '; ' 'N=' num2str(N) ...
+                '; {\color{red}total=' num2str(P) '})'],...
+                'Parent',ax_h(plotCnt));
+        else
+            title([siteName ' (' siteIDs{k} ' & ' char(comSID) ...
+                '; ' 'N=' num2str(N) ...
+                ')'],...
+                'Parent',ax_h(plotCnt));
+        end
+        
+    else
+        if ne(P,N)
+            title([siteName ' (' siteIDs{k} '; N=' num2str(N) ...
+                '; {\color{red}total=' num2str(P) '})'],...
+                'Parent',ax_h(plotCnt));
+        else
+            title([siteName ' (' siteIDs{k} '; N=' num2str(N) ...
+                ')'],...
+                'Parent',ax_h(plotCnt));
+        end
+    end
+    
+    plotCnt = plotCnt+1;
+    %% fall
+    useI = true(length(yrStripQ),1);
+    useIp = ge(yrStripP,datenum('0-09-22','yyyy-mm-dd')) & ...
+        lt(yrStripP,datenum('0-12-21','yyyy-mm-dd'));
+    useIs = ge(yrStripS,datenum('0-09-22','yyyy-mm-dd')) & ...
+        lt(yrStripS,datenum('0-12-21','yyyy-mm-dd'));
+    Qtmp = Qdaily(useI);
+    Qdts = Qdates(useI);
+    [srtQ,srtI] = sort(-Qtmp); % reverse sort
+    srtQ = -srtQ;
+    Qdts = Qdts(srtI);
+    tDts = dates(useIp);
+    
+    xNumz = linspace(0,100,length(srtQ));
+    plot(xNumz,srtQ,'Parent',ax_h(plotCnt),'LineWidth',LW)
+    % now...find the sampling locations
+    
+    N = 0;
+    for j = 1:length(tDts)
+        plotI = eq(tDts(j),Qdts);
+        plot(xNumz(plotI),srtQ(plotI),'ro',...
+            'MarkerSize',mS,'MarkerFaceColor',mF,...
+            'Parent',ax_h(plotCnt),'LineWidth',.75)
+        N = N + sum(plotI);
+        
+    end
+    % GLRI range plot
+    useI = ge(Qdates,GLRI_st) & lt(Qdates,GLRI_en);
+    mxI  = max(Qdaily(useI));
+    mnI  = min(Qdaily(useI));
+    
+    plotI_1 = find(srtQ==mxI,1,'first');
+    plotI_2 = find(srtQ==mnI,1,'first');
+    plot(xNumz(plotI_1),mxI,'kv','Parent',ax_h(plotCnt),'LineWidth',.75,...
+        'MarkerFaceColor','w','MarkerSize',6)
+    plot(xNumz(plotI_2),mnI,'k^','Parent',ax_h(plotCnt),'LineWidth',.75,...
+        'MarkerFaceColor','w','MarkerSize',6)
+    
+    mxI  = max(seasonTrim(useIs));
+    mnI  = min(seasonTrim(useIs));
+    plotI_1 = find(srtQ==mxI,1,'first');
+    plotI_2 = find(srtQ==mnI,1,'first');
+    plot(xNumz(plotI_1),mxI,'kx','Parent',ax_h(plotCnt),'LineWidth',1.25,...
+        'MarkerSize',9)
+    plot(xNumz(plotI_2),mnI,'kx','Parent',ax_h(plotCnt),'LineWidth',1.25,...
+        'MarkerSize',9)
+    P = length(tDts);
+    siteName = 'Fall';
+    if gage
+        set(get(ax_h(plotCnt),'YLabel'),'String','Gage height (ft)')
+        set(ax_h(plotCnt),'YScale','linear')
+    end
+    if cmb
+        if ne(P,N)
+            title([siteName ' (' siteIDs{k} ' & ' char(comSID) ...
+                '; ' 'N=' num2str(N) ...
+                '; {\color{red}total=' num2str(P) '})'],...
+                'Parent',ax_h(plotCnt));
+        else
+            title([siteName ' (' siteIDs{k} ' & ' char(comSID) ...
+                '; ' 'N=' num2str(N) ...
+                ')'],...
+                'Parent',ax_h(plotCnt));
+        end
+        
+    else
+        if ne(P,N)
+            title([siteName ' (' siteIDs{k} '; N=' num2str(N) ...
+                '; {\color{red}total=' num2str(P) '})'],...
+                'Parent',ax_h(plotCnt));
+        else
+            title([siteName ' (' siteIDs{k} '; N=' num2str(N) ...
+                ')'],...
+                'Parent',ax_h(plotCnt));
+        end
+    end
+    
+    
+    
     if eq(plotCnt,plotGen) || eq (k,numSites)
         for pl = plotCnt+1:plotGen
             delete(ax_h(pl));
         end
-        figTitle = [figDir 'FlowDur_0' num2str(ceil(k/plotGen))];
+        figTitle = [rootDir figFldr siteIDs{k} '_seasonFlowDur'];
         disp(figTitle)
         export_fig(figTitle,'-png','-m1','-nocrop')
         plotCnt = 0;
